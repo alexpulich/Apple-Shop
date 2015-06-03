1ea355aa07e19ab6529392fe8e1d36d18da6dca4
Alex Pulich <alexpulich@gmail.com>
2015-06-03 03:27:09 +0300

    bug fixes

--- app/js/main.js
+++ app/js/main.js
@@ -1,135 +1,144 @@
-var Slideshow = function() {
-	var
-		_setUpListeners = function() {
-			$('.prev').on('click', _prevPhoto);
-			$('.next').on('click', _nextPhoto);
-			$('.slideshow__pic').on('click', _changePhoto);
-		},
+var Slider = function() {
+        var
+            _moveSlide = function($this, direction, callback) {
+                var
+                    container = $this.closest('.slideshow__slider'),
+                    list = container.find('.slideshow__list'),
+                    activeSlide = list.find('.slideshow__item.active')
+                duration = 300,
+                    activeSlideSibling = (direction === 'next') ? activeSlide.next() : activeSlide.prev(),
+                    reqPosition = (activeSlideSibling.length) ? activeSlideSibling.position().left : '',
+                    edgesElements = (direction === 'next') ? activeSlide.nextAll().eq(2) : activeSlide.prev();
+                underEdgesElements = (direction === 'next') ? edgesElements.next() : edgesElements.prev();
 
-		_nextPhoto = function(e) {
-			e.preventDefault();
+                if (edgesElements.length) {
+                    list.animate({
+                        left: -reqPosition
+                    }, duration, function() {
+                        activeSlide.removeClass('active');
+                        activeSlideSibling.addClass('active');
 
-			var	
-				$this = $(this),
-				container = $this.closest('.slideshow'),
-				display = container.find('.slideshow__display'),
-				slideshowList = $this.closest('.slideshow__list'),
-				activeItem = slideshowList.find('.active'),
-				nextItem = activeItem.next('.slideshow__item'),
-				path = nextItem.find('img').attr('src'),
-				duration = 300;
+                        if (!underEdgesElements.length) {
+                            callback.call($this);
+                        }
 
-			if (nextItem.val() !== undefined ) {
-				activeItem.removeClass('active');			
-				nextItem.addClass('active');
-				display.find('img').fadeOut(duration, function() {
-					$(this).attr('src', path).fadeIn(duration);
-				});
-			} 
-		},
+                    });
+                }
+            };
 
-		_prevPhoto = function(e) {
-			e.preventDefault();
+        return {
+            init: function() {
+                $('.slideshow__nav-link').on('click', function(e) {
+                    e.preventDefault();
+                    var
+                        $this = $(this),
+                        direction = ($this.hasClass('next')) ? 'next' : 'prev';
+                    _moveSlide($this, direction, function() {
+                        var
+                            oppositeDirection = (direction === 'next') ? 'prev' : 'next';
+                        $('.slideshow__nav-link.' + direction).hide();
+                        $('.slideshow__nav-link.' + oppositeDirection).show();
+                    });
 
-			var	
-				$this = $(this),
-				container = $this.closest('.slideshow'),
-				display = container.find('.slideshow__display'),
-				slideshowList = $this.closest('.slideshow__list'),
-				activeItem = slideshowList.find('.active'),
-				prevItem = activeItem.prev('.slideshow__item'),
-				path = prevItem.find('img').attr('src'),
-				duration = 300;
+                    $this.siblings().show();
+                });
+            }
+        }
+    }(),
 
-			if (prevItem.val() !== undefined ) {
-				activeItem.removeClass('active');			
-				prevItem.addClass('active');
-				display.find('img').fadeOut(duration, function() {
-					$(this).attr('src', path).fadeIn(duration);
-				});
-			}
-		},
+    Slideshow = function() {
+        var
+            _changePic = function($this) {
+                var
+                    path = $this.data('fullsize'),
+                    container = $this.closest('.slideshow');
+                display = $('.slideshow__display img'),
+                    duration = 300;
+                console.log(display);
+                display.fadeOut(duration, function() {
+                    $(this).attr('src', path);
 
-		_changePhoto = function(e) {
-			e.preventDefault();
+                    $(this).on('load', function() {
+                        $(this).fadeIn(duration);
+                    });
+                });
+            };
 
-			var
-				$this = $(this),
-				item = $this.closest('.slideshow__item'),
-				container = $this.closest('.slideshow'),
-				display = container.find('.slideshow__display'),
-				path = item.find('img').attr('src'),
-				duration = 300;
+        return {
+            init: function() {
+                $('.slideshow__pic').on('click', function(e) {
+                    e.preventDefault();
+                    _changePic($(this));
+                })
+            }
+        }
+    }(),
 
-			if (!item.hasClass('active')) {
-				item.addClass('active').siblings().removeClass('active');
-				display.find('img').fadeOut(duration, function() {
-					$(this).attr('src', path).fadeIn(duration);
-				});
-			}
-		};
+    ScrollToTop = function() {
+        var
+            _goUp = function(e) {
+                e.preventDefault();
 
-	return {
-		init: _setUpListeners
-	}
-}(),
+                $('body,html').animate({
+                    scrollTop: 0
+                }, 400);
+            };
 
-ScrollToTop = function() {
-	var 
-		_setUpListeners = function() {
-			$('.up').on('click', _goUp)
-		},
+        return {
+            init: function() {
+                $(window).scroll(function() {
+                    if ($(this).scrollTop() > 0) {
+                        $('.up').fadeIn();
+                    } else {
+                        $('.up').fadeOut();
+                    }
+                });
+            },
+            listening: function() {
+                $('.up').on('click', _goUp);
+            }
+        }
 
-		_init = function() {
-			$(window).scroll(function() {
-				if ($(this).scrollTop() > 0) {
-					$('.up').fadeIn();
-				} else {
-					$('.up').fadeOut();
-				}
-			});
-		}
+    }(),
 
-		_goUp = function(e) {
-			e.preventDefault();
+    IE = function() {
 
-			$('body,html').animate({
-				scrollTop: 0
-			}, 400);
-		};
+        return {
+            init: function() {
+                $('.about-info__column:last-child').css("margin-right", "0");
+                $('.user-nav__item:last-child').css("margin-right", "0");
+                $('.bread-crumbs__link:last-child').css({
+                    "color": "#5c7b98",
+                    "text-decoration": "none"
+                });
+                $('.subnav__list:last-child').css("margin-right", "0");
+                $('.slideshow__item:last-child').css("margin-right", "0");
+                $('.subsocials__item:last-child').css("margin-right", "0");
+                $('input[type="search"').css('padding-top', '10px');
+                $('.desctable__row:nth-child(2n)').css("background", "#eef2f4");
+            }
+        }
+    }();
 
-	return {
-		init: _init,
-		listening: _setUpListeners
-	}
+$(document).ready(function() {
+    if ($('.slideshow__list').length) {
+        Slider.init();
+    }
 
-}(),
+    if ($('.slideshow').length) {
+        Slideshow.init();
+    }
 
-IE = function() {
-	var
-		_fixLastChild = function() {
-			if (/msie [1-8]{1}[^0-9]/.test(navigator.userAgent.toLowerCase())) {
-				$('.about-info__column:last-child').css("margin-right", "0");
-				$('.user-nav__item:last-child').css("margin-right", "0");
-				$('.bread-crumbs__link:last-child').css({
-					"color": "#5c7b98",
-					"text-decoration":"none"
-				});
-				$('.subnav__list:last-child').css("margin-right", "0");
-				$('.slideshow__item:last-child').css("margin-right", "0");
-				$('.subsocials__item:last-child').css("margin-right", "0");
-				$('input[type="search"').css('padding-top','10px');
-				$('.desctable__row:nth-child(2n)').css("background", "#eef2f4");
-			}
-		};
+    if ($('.up').length) {
+        ScrollToTop.init();
+        ScrollToTop.listening();
+    }
 
-	return {
-		init: _fixLastChild
-	}
-}();
+    if (/msie [1-8]{1}[^0-9]/.test(navigator.userAgent.toLowerCase())) {
+        IE.init();
+    }
 
-Slideshow.init();
-ScrollToTop.init();
-ScrollToTop.listening();
-IE.init();
-$('input, textarea').placeholder();
+    if ($('input, textarea')) {
+        $('input, textarea').placeholder();
+    }
+})
